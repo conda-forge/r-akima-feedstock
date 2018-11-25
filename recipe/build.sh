cd4c506019022ad9b1d54bd9f72c0d6a599b84c5
Devon Ryan <dpryan79@gmail.com>
2018-11-25 23:55:35 +0100

    Only on OSX, apparently flags need to be adjusted otherwise

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-mkdir -p ~/.R
-echo -e "CC=$CC\nF77=$F77\n" > ~/.R/Makevars
+if [[ $target_platform =~ linux.* ]]; then
+    mkdir -p ~/.R
+    echo -e "CC=$CC\nF77=$F77\n" > ~/.R/Makevars
+fi
 $R CMD INSTALL --build .
